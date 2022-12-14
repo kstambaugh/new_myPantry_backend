b0e8cec0a34d6717e316b63bcb02f0a4d90c5461
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-12-13 19:59:42 -0500

    making routes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
-const dotevn = require('dotenv')
-dotevn.config()
-
-
+const dotenv = require('dotenv')
 const app = express();
+dotenv.config()
+
+
+app.use('/users', require('./controllers/users'))
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
 
+app.get("/*", (req, res) => {
+    res.status(404).send("<h1>404 Page</h1>")
+})
 
-app.listen(5000, () => console.log("Sever is listening on port 5000"))
+
+
+app.listen(process.env.PORT, () => console.log("Sever is listening on port 5000"))
